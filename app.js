7680989ca6c6f6cfa2ef4b98b8098a27b7c3771a
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-17 00:00:25 +0530

    :sparkles: added middlewares

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ import router from "./routes/user-routes";
 
 const app=express();
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>res.send('Hello BlogAPI_Task'));
 
 app.use("/user",router);
-app.use("/blogs",blogrouter)
+app.use("/blogs",blogrouter);
 
 mongoose.connect("mongodb://localhost:27017/Blog")
 .then(()=> app.listen(5000))
-.then(()=>console.log("conn to 5000"))
-.catch((err)=>console.log(err))
+.then(()=>console.log("Server is connected to port 5000"))
+.catch((err)=>console.log(err));
