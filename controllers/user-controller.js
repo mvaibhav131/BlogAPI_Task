097bb6c86b0dc85392a8dc50c3107f7d4164b3cc
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-17 16:16:57 +0530

    :sparkles: done blog and user function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,62 +1,61 @@
 import User from "../models/User";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
 
-export const getAllUser = async (req,res,next)=>{
-    let users;
-    try{
-        users=await User.find();
-    }catch (err){
-      return console.log(err);
-    }
-    if(!users){
-        return res.status(404).json({message:"No Users Found"});
-    }
-    return res.status(200).json({users});
+export const getAllUser = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!users) {
+    return res.status(404).json({ message: "No Users Found" });
+  }
+  return res.status(200).json({ users });
 };
 
-export const signup=async (req,res,next)=>{
-    const {name,email,password}=req.body;
-    let existingUser;
-    try{
-        existingUser=await User.findOne({email});
-    }catch(err){
-       return  console.log(err);
-     }
-     if(existingUser){
-        return res.status(400).json({message:"User Already exist"})
-     }
-     const hashedPassword=bcrypt.hashSync(password);
-     const user=new User({
-        name,
-        email,
-        password:hashedPassword,
-        blogs:[],
-     });
-     try{
-       await user.save();
-     }catch(err){
-       return console.log(err);
-     }
-     return res.status(201).json({user})
+export const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (existingUser) {
+    return res.status(400).json({ message: "User Already exist" });
+  }
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
+    blogs: [],
+  });
+  try {
+    await user.save();
+  } catch (err) {
+    return console.log(err);
+  }
+  return res.status(201).json({ user });
 };
 
-export const login=async (req,res,next)=>{
-    const {email,password}=req.body;
-    let existingUser;
-    try{
-        existingUser=await User.findOne({email});
-    }catch(err){
-       return  console.log(err);
-     }
-     if(!existingUser){
-        return res.status(404).json({message:"Couldn't find user"});
-     }
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!existingUser) {
+    return res.status(404).json({ message: "Couldn't find user" });
+  }
 
-    //  const ispasswordcorrect=await User.findOne({password});
-    const isPasswordCorrect=bcrypt.compareSync(password,existingUser.password);
-     if(!isPasswordCorrect){
-        return res.status(400).json({message:"Incorrect Password"});
-
-     }
-     return res.status(200).json({message:"Login Successfull"});
+  //  const ispasswordcorrect=await User.findOne({password});
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+  return res.status(200).json({ message: "Login Successfull" });
 };
