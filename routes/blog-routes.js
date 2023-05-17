e2f5016d16429d0b87c4919499b88f82c1f53056
vaibhav more <98808183+mvaibhav131@users.noreply.github.com>
2023-05-17 16:17:24 +0530

    :sparkles: routes for blog and user done

--- routes/blog-routes.js
+++ routes/blog-routes.js
@@ -1,14 +1,21 @@
 import express from "express";
-import { getAllBlogs,addBlogs, updateblog, getById, deleteBlog, getByUserId, getByKeyword } from "../controllers/blog-controller";
-const blogrouter=express.Router();
+import {
+  getAllBlogs,
+  addBlogs,
+  updateblog,
+  getById,
+  deleteBlog,
+  getByUserId,
+  getByKeyword,
+} from "../controllers/blog-controller";
+const blogrouter = express.Router();
 
-
-blogrouter.get("/",getAllBlogs); // Get all blogs
-blogrouter.post("/add",addBlogs); // Create new blogs
-blogrouter.put("/update/:id",updateblog); // Update blogs according to id
-blogrouter.get("/:id",getById); //Get blog by id 
-blogrouter.delete("/:id",deleteBlog); //Delete blog by id
-blogrouter.get("/user/:id",getByUserId); //Get blog by userID
-blogrouter.post("/search",getByKeyword); // Search blog according to keywords
+blogrouter.get("/", getAllBlogs); // Get all blogs
+blogrouter.post("/add", addBlogs); // Create new blogs
+blogrouter.put("/update/:id", updateblog); // Update blogs according to id
+blogrouter.get("/:id", getById); //Get blog by id
+blogrouter.delete("/:id", deleteBlog); //Delete blog by id
+blogrouter.get("/user/:id", getByUserId); //Get blog by userID
+blogrouter.post("/search", getByKeyword); // Search blog according to keywords
 
 export default blogrouter;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,10 +1,10 @@
-import express from "express"
-import { getAllUser, login, signup }  from "../controllers/user-controller";
+import express from "express";
+import { getAllUser, login, signup } from "../controllers/user-controller";
 
-const router=express.Router();
+const router = express.Router();
 
-router.get("/",getAllUser ); // Get all user list 
-router.post("/signup",signup); // Sign up user
-router.post("/login",login); // Login User
+router.get("/", getAllUser); // Get all user list
+router.post("/signup", signup); // Sign up user
+router.post("/login", login); // Login User
 
-export default router;  
+export default router;
